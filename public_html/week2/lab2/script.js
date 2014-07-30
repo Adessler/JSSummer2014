f9f10138343b9e4cbd342783187783188b5c5126
001292266 <001292266@N205A1.labs.neit.edu>
2014-07-29 20:05:12 -0400

    week 1 demo

---
+++ public_html/week2/lab2/script.js
@@ -0,0 +1,48 @@
+function submitForm() {
+    
+    var fullname = document.getElementById("name");
+    var fullnameErr = document.getElementById("err_name");
+    
+    var hasErrors = false;
+    
+    if ( fullname.value.length ) {
+        fullname.classList.remove('bad');
+        fullname.classList.add('good');        
+        fullnameErr.innerHTML = '';
+        
+    } else {
+        hasErrors = true;
+        fullname.classList.remove('good');
+        fullname.classList.add('bad');       
+        fullnameErr.innerHTML = "<p>Full Name is not valid.</p>";       
+    }
+    
+    if (email.value.length){
+        email.classList.add('good');
+        email.classList.remove('bad');
+        email.innerHTML = '';
+    }else {
+        hasErrors= true;
+        email.classList.add('bad');
+        email.classList.remove('good');
+      email.innerHTML = "<p>Email is not valid.</p>";
+    }
+        
+    if(comments.value.length){
+        comments.classList.add('good');
+        comments.classList.remove('bad');
+        comments.innerHTML = '';
+    }    
+    if (comments.value.length){
+        hasErrors=true;
+        comments.classList.add('bad');
+        comments.classList.remove('good');
+        comments.innerHTML = "<p>Comments not valid.</p>";
+    }
+        
+    
+        }
+    
+    
+ 
+
